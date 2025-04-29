cc761f702c004b4ce30b7dd676f7469a00e8aced
Yupi <youngperchik2404@gmail.com>
2025-04-29 22:02:13 +0500

    Added class for serializing information from API to code

---
+++ app/src/main/java/com/example/eventscalendar/Event.java
@@ -0,0 +1,83 @@
+package com.example.eventscalendar;
+
+import android.location.Location;
+
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+
+public class Event {
+
+    @SerializedName("name")
+    private String name;
+
+    @SerializedName("url")
+    private String url;
+
+    @SerializedName("starts_at")
+    private String startsAt;
+
+    @SerializedName("location")
+    private Location location;
+
+    public Event(String name, String url, String startsAt, Location location) {
+        this.name = name;
+        this.url = url;
+        this.startsAt = startsAt;
+        this.location = location;
+    }
+
+    // Геттеры и сеттеры
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getStartsAt() {
+        return startsAt;
+    }
+
+    public void setStartsAt(String startsAt) {
+        this.startsAt = startsAt;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    // Метод для сериализации объекта в JSON
+    public String toJson() {
+        Gson gson = new Gson();
+        return gson.toJson(this);
+    }
+
+    // Метод для десериализации объекта из JSON
+    public static Event fromJson(String json) {
+        Gson gson = new Gson();
+        return gson.fromJson(json, Event.class);
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "name='" + name + '\'' +
+                ", url='" + url + '\'' +
+                ", startsAt='" + startsAt + '\'' +
+                ", location=" + location +
+                '}';
+    }
+}
